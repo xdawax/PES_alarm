522dae1a988e71e66a9f07ce2ad7b696b26d48cd
xdawax <david.carlsson.6652@student.uu.se>
2019-04-17 20:22:47 +0200

    Initial commit

---
+++ RTE/_STM32F103C8/RTE_Components.h
@@ -0,0 +1,33 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'PES_alarm' 
+ * Target:  'STM32F103C8' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_RTC
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_DEVICE_STDPERIPH_USART
+#define RTE_RTOS_FreeRTOS_CONFIG_RTOS2  /* RTOS FreeRTOS Config for CMSIS RTOS2 API */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+
+#endif /* RTE_COMPONENTS_H */
