7da8e31699862ef3be2081cf9581ca353ea50a27
xdawax <david.carlsson.6652@student.uu.se>
2019-04-24 08:09:00 +0200

    Add button functionallity

--- src/main.c
+++ src/main.c
@@ -5,16 +5,10 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-// Register defines -> h-file
-#define USART_UE 13
-#define USART_M 12
-#define USART_DMAT 7
-#define USART_TE 3
-#define USART_TC 6
-
 
 void ledInit(void);
 void usartInit(void);
+void buttonInit(void);
 // RTOS task
 void vTaskLedRed(void *p);
 void vTaskLedYellow(void *p);
@@ -23,18 +17,44 @@ void vTaskLedGreen(void *p);
 int main(void)
 {
     // Configure GPIO for LED
+		buttonInit();
     ledInit();
 		usartInit();
-    // Create LED blink task
+  /*  // Create LED blink task
     xTaskCreate(vTaskLedRed, (const char*) "Red LED Blink", 
         128, NULL, 1, NULL);
     xTaskCreate(vTaskLedYellow, (const char*) "Yellow LED Blink",
         128, NULL, 1, NULL);
     xTaskCreate(vTaskLedGreen, (const char*) "Green LED Blink", 128, NULL, 1, NULL);
-    //Start RTOS scheduler
+    //Start RTOS scheduler*/
     vTaskStartScheduler();
 
+		while (1) {
+			
+        if (!(GPIO_ReadInputData(GPIOB) & GPIO_Pin_12))
+        {
+            // Turn on LED on PA0 (LED circuit is active low)
+            GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+        }
+        else
+        {
+            // Turn off LED on PA0
+            GPIO_SetBits(GPIOB, GPIO_Pin_13);
+        }
+		}
+		
     return 0;
+}
+
+void buttonInit()
+{
+		GPIO_InitTypeDef GPIO_InitStruct;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
 void ledInit()
@@ -43,11 +63,17 @@ void ledInit()
 
     // Configure PC13, PC14, PC15 as push-pull output
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
     GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Init(GPIOC, &GPIO_InitStruct);
-}
+	
+		GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
+		GPIO_Init(GPIOB, &GPIO_InitStruct);
+		GPIO_SetBits(GPIOB, GPIO_Pin_13);
+	}
 
 void usartInit()
 {
@@ -62,9 +88,9 @@ void usartInit()
 		USART1->CR1 |= USART_CR1_UE;							// enable
 		USART1->CR1 |= USART_CR1_M;								// word length 8 bit
 		USART1->CR2 &= ~((1 << 13) | (1 << 12));	// bit 12-13 == 0,0 => 1 stop bit
-		USART1->CR3 |= USART_CR3_DMAT;
+		//USART1->CR3 |= USART_CR3_DMAT;					// DMA enable	
 		USART1->BRR = 0x1d4c;											// mantissa 468, fraction 0,75 => 111010100, 1100 (72MHz)
-		USART1->CR1 |= USART_CR1_TE;							// tx enable
+		USART1->CR1 |= USART_CR1_TE | USART_CR1_RE;							// tx enable
 }
 
 void vTaskLedRed(void *p)
@@ -72,9 +98,6 @@ void vTaskLedRed(void *p)
 	  for (;;)
     {
         GPIOC->ODR ^= GPIO_Pin_14;
-				
-				USART1->DR = (uint16_t)'a';
-				while (!(USART1->SR & USART_SR_TXE)){}; // empty
         vTaskDelay(100/portTICK_RATE_MS);
     }
 }
@@ -83,8 +106,16 @@ void vTaskLedYellow(void *p)
 {
     for (;;)
     {
+				USART1->DR = (uint16_t)'A';
+				while (!(USART1->SR & USART_SR_TXE)){}; // empty
+				USART1->DR = (uint16_t)'T';
+				while (!(USART1->SR & USART_SR_TXE)){}; // empty
+				USART1->DR = (uint16_t)'\r';
+				while (!(USART1->SR & USART_SR_TXE)){}; // empty
+				USART1->DR = (uint16_t)'\n';
+				while (!(USART1->SR & USART_SR_TXE)){}; // empty	
         GPIOC->ODR ^= GPIO_Pin_15;
-        vTaskDelay(500/portTICK_RATE_MS);
+        vTaskDelay(1000/portTICK_RATE_MS);
     }
 }
 
