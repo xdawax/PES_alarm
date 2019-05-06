de03708c95f85ef90d6bf6f3aa632d1de0aa297e
xdawax <david.carlsson.6652@student.uu.se>
2019-05-06 08:52:43 +0200

    Add timestamp struct

--- header/rtc.h
+++ header/rtc.h
@@ -1,4 +1,13 @@
 #ifndef RTC_H_
 #define RTC_H_
 
+#include <stdint.h>
+
+typedef struct time_stamp {
+	uint8_t month;
+	uint8_t day;
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+} time_stamp_t;
 #endif
